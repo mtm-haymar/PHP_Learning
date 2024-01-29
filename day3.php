b68044088f717696bdffc2ad414e25c5246f67e9
mtm-haymar <haymar@metateammyanmar.com>
2024-01-29 21:02:48 +0630

    [day3]

---
+++ day3.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 5;
+$y = "John";
+
+echo $x;
+echo "<br>";
+echo $y;
+
+
+echo "<br>";
+//If statement
+if (3<4) {
+    echo "Have a good Day";
+}
+
+?>
+
+</body>
+</html>
